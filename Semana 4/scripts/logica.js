e08b7570ba5456ec6005f7c91b981e83df211e96
jmcordoba@universidadean.edu.co <jmcordoba@universidadean.edu.co>
2018-02-20 13:07:52 -0500

    Add hasta ejercicio triangulos

--- Semana 4/scripts/logica.js
+++ Semana 4/scripts/logica.js
@@ -24,3 +24,53 @@ if (!boleano) {
 } else {
     console.log("opcion verdadera");
 }
+
+// podemos crear arreglos
+var tequilas = ['jose cuervo', 'olmeca', '1800', 'maestro tequilero', 'cabrito', 'la herradura', 'agave azul'];
+
+//Y podemos recorrerlos con el ciclo for
+for(var i=0;i<tequilas.length; i++){
+    console.log(tequilas[i]);
+}
+
+//o vaciarlos como una pila usando while
+while(tequilas.length>0){
+    console.log(tequilas.pop());
+}
+
+/**
+ * Calcula el perimetro de un triangulo
+ * @param   {Number} lado1 el primer lado del triangulo
+ * @param   {Number} lado2 el segundo lado del triangulo
+ * @param   {Number} lado3 el tercer lado del triangulo
+ * @returns {Number} El perimetro del triangulo
+ */
+function perimetroTriangulo(lado1, lado2, lado3){
+    return lado1 + lado2 + lado3;
+}
+
+/**
+ * Calcula el semiperimetro de un triangulo
+ * @param   {Number} lado1 el primer lado del triangulo
+ * @param   {Number} lado2 el segundo lado del triangulo
+ * @param   {Number} lado3 el tercer lado del triangulo
+ * @returns {Number} El semiperimetro del triangulo
+ */
+function semiperimetroTriangulo(lado1,lado2,lado3){
+    return perimetroTriangulo(lado1,lado2,lado3)/2;
+}
+
+/**
+ * Calcula el area de un triangulo
+ * @param   {Number} lado1 el primer lado del triangulo
+ * @param   {Number} lado2 el segundo lado del triangulo
+ * @param   {Number} lado3 el tercer lado del triangulo
+ * @returns {Number} El area del triangulo
+ */
+function areaTriangulo(lado1,lado2,lado3){
+    var semiperimetro = semiperimetroTriangulo(lado1,lado2,lado3);
+    var s_lado1 = semiperimetro - lado1;
+    var s_lado2 = semiperimetro - lado2;
+    var s_lado3 = semiperimetro - lado3;
+    return (semiperimetro*s_lado1*s_lado2*s_lado3)**(1/2)
+}
