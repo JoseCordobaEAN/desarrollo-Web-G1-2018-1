503cae2b3cfa1cc502fdaa7f5b0c80fd255bc3e9
jmcordoba@universidadean.edu.co <jmcordoba@universidadean.edu.co>
2018-02-20 12:10:38 -0500

    Add ejercicios semana 4

---
+++ Semana 4/scripts/logica.js
@@ -0,0 +1,26 @@
+/**
+Ejemplo de JavaScript para la clase de desarrollo web grupo 1 de la universidad EAN
+@author: Jose Cordoba
+@since: 20/02/2018
+*/
+//La sintaxis de java script es similar
+//a la de C o Java
+
+//Podemos escribir en la consola
+console.log("Hola mundo");
+
+//O podemos escribir en una alerta
+alert("Hola mundo");
+
+//Para escribir en el documento
+document.write("<h1>Hola Mundo</h1>");
+
+//podemos crear varibles
+var boleano = true;
+
+//También podemos usar estructuras de decisión
+if (!boleano) {
+    console.log("opcion falsa");
+} else {
+    console.log("opcion verdadera");
+}
